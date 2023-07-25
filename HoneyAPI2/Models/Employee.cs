1034ff29ac6c235ec481d7e88830732ed3d44d06
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-07-24 20:02:45 -0500

    Add project files.

---
+++ HoneyAPI2/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace HoneyAPI2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+    }
+}
---
+++ HoneyAPI2/Program.cs
@@ -0,0 +1,155 @@
+using HoneyAPI2.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+List<Customer> customers = new List<Customer> { };
+List<Employee> employees = new List<Employee> { };
+List<ServiceTicket> serviceTickets = new List<ServiceTicket> { };
+
+//Create lists
+List<Customer> customerList = new List<Customer> ()
+{
+    new Customer { 
+        Id = 1,
+        Name = "Sterling",
+        Address = "123 Street"
+},
+    new Customer { 
+        Id = 2,
+        Name = "Foden",
+        Address = "456 Road"
+},
+    new Customer {
+        Id = 3,
+        Name = "Henderson",
+        Address = "789 Circle"
+}
+};
+
+List<Employee> employeeList = new List<Employee>()
+{
+    new Employee
+    {
+        Id = 4,
+        Name = "Jack",
+        Specialty = "Food"
+    },   
+    new Employee
+    {
+        Id = 5,
+        Name = "Lauren",
+        Specialty = "Manager"
+    },
+
+};
+
+List<ServiceTicket> serviceTicketsList = new List<ServiceTicket>()
+{
+    new ServiceTicket
+    {
+        Id = 5,
+        CustomerId = 2,
+        EmployeeId = 4,
+        Description = "Office Party",
+        Emergency = false,
+        DateCompleted = "2023-07-01"
+    },    
+    new ServiceTicket
+    {
+        Id = 6,
+        CustomerId = 1,
+        EmployeeId = 5,
+        Description = "Budget Review",
+        Emergency = false,
+        DateCompleted = "2023-07-05"
+    },    
+    new ServiceTicket
+    {
+        Id = 7,
+        CustomerId = 3,
+        Description = "Cleaning",
+        Emergency = true,
+    },    
+    new ServiceTicket
+    {
+        Id = 8,
+        CustomerId = 2,
+        EmployeeId = 5,
+        Description = "Stock",
+        Emergency = true,
+    },   
+    new ServiceTicket
+    {
+        Id = 9,
+        CustomerId = 3,
+        Description = "Mail",
+        Emergency = false,
+
+    },
+
+};
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/servicetickets", () =>
+{
+    return serviceTicketsList;
+});
+
+app.MapGet("/servicetickets/{id}", (int id) =>
+{
+    ServiceTicket serviceTicket = serviceTicketsList.FirstOrDefault(st => st.Id == id);
+    if (serviceTicket == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(serviceTicket);
+});
+
+app.MapGet("/employees", () =>
+{
+    return employeeList;
+});
+
+app.MapGet("/employees/{id}", (int id) =>
+{
+    Employee employee = employeeList.FirstOrDefault(e => e.Id == id);
+    if (employee == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(employee);
+
+});app.MapGet("/customers", () =>
+{
+    return customerList;
+});
+
+app.MapGet("/customers/{id}", (int id) =>
+{
+    Customer customer = customerList.FirstOrDefault(c => c.Id == id);
+    if (customer == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(customer);
+});
+
+app.Run();
